678015961a54f5f8d32c57af2d8641103cadfba8
SirDaechel <anayookpala26@gmail.com>
2024-07-29 13:38:30 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,62 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+      saveDeployments: true,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    // token: "MATIC",
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: ["Raffle"],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  solidity: "0.8.24",
+  mocha: {
+    timeout: 500000, // 500 seconds max for running tests
+  },
+};
---
+++ helper-hardhat-config.js
@@ -0,0 +1,38 @@
+const { ethers } = require("hardhat");
+
+const networkConfig = {
+  default: {
+    name: "hardhat",
+    keepersUpdateInterval: "30",
+  },
+  31337: {
+    name: "localhost",
+    subscriptionId: "588",
+    gasLane:
+      "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 100 gwei
+    keepersUpdateInterval: "30",
+    raffleEntranceFee: ethers.utils.parseEther("0.01"),
+    callbackGasLimit: "500000", // 500,000 gas
+  },
+  11155111: {
+    name: "sepolia",
+    subscriptionId: "6926",
+    gasLane:
+      "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae", // 100 gwei
+    keepersUpdateInterval: "30",
+    raffleEntranceFee: ethers.utils.parseEther("0.01"),
+    callbackGasLimit: "500000",
+    vrfCoordinatorV2: "0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B",
+  },
+  1: {
+    name: "mainnet",
+    keepersUpdateInterval: "30",
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+};
